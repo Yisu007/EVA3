cbe8da1f7f2c68701e8b2d7aeabdc5f5164acacd
Yisu007 <L24550246@chihuahua2.tecnm.mx>
2024-11-04 17:42:16 -0600

    Add files via upload

---
+++ EVA3_METODOS/src/eva3_metodos/EVA3_METODOS.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_metodos;
+
+/**
+ *
+ * @author Xxj53
+ */
+public class EVA3_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        //bloque de codigo .{
+        //instrucciones 
+    //}
+    //organizar y reutilizar un codigo 
+    //perteneece a una clase 
+    //USO DE LOS METODOS:
+    //INVOCACION O LLAMADA 
+    imprimirMensaje("hola mundo!!");
+    }
+    //header (encabezado)
+    //imprimir un mensaje
+    //JAVA: iniciar en minusculas,verbo, siguientes palabras inician en mayusculas 
+    
+    public static void imprimirMensaje(String mensaje ){
+        System.out.println("TU mensaje :" + mensaje );
+        
+    }
+}
